477f6225b21f0f20c934dedc0184954dcbb89047
CarlJoelWester <108466352+CarlJoelWester@users.noreply.github.com>
2023-11-20 23:22:48 +0100

    Update script.js

--- script.js
+++ script.js
@@ -19,7 +19,6 @@ async function submitQuestion() {
         const data = await response.json();
         responseDiv.innerText = data.answer;
 
-        // Save the question and response
         conversationHistory.push({
             question: question,
             answer: data.answer
@@ -32,33 +31,18 @@ async function submitQuestion() {
     }
 }
 
+function downloadCSV() {
+    // Implement downloadCSV functionality here
+}
+
 function displayConversations() {
-    const existingPanel = document.getElementById('conversation-panel');
-    if (existingPanel) {
-        existingPanel.style.display = 'block'; // Show the panel if it already exists
-    } else {
-        // Create the panel
-        const panel = document.createElement('div');
-        panel.id = 'conversation-panel';
-        panel.style.cssText = `
-            position: fixed;
-            right: 0;
-            top: 0;
-            width: 300px;
-            height: 100vh;
-            background-color: white;
-            box-shadow: -2px 0px 5px rgba(0, 0, 0, 0.2);
-            padding: 20px;
-            overflow-y: auto;
-            display: block;
-        `;
+    const panel = document.getElementById('conversation-panel');
+    panel.style.display = 'block';
 
-        let content = '<h3>Conversations</h3>';
-        conversationHistory.forEach(({ question, answer }) => {
-            content += `<p><strong>Q:</strong> ${question}<br><strong>A:</strong> ${answer}</p>`;
-        });
+    let content = '<h3>Conversations</h3>';
+    conversationHistory.forEach(({ question, answer }) => {
+        content += `<p><strong>Q:</strong> ${question}<br><strong>A:</strong> ${answer}</p>`;
+    });
 
-        panel.innerHTML = content;
-        document.body.appendChild(panel);
-    }
+    panel.innerHTML = content;
 }
