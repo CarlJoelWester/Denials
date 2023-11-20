fa5bb4bad0e9975ea81134dbd9cf1c6d3ab3bca0
CarlJoelWester <108466352+CarlJoelWester@users.noreply.github.com>
2023-11-20 22:52:59 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,23 @@
+async function submitQuestion() {
+    const question = document.getElementById('question').value;
+    const responseParagraph = document.getElementById('response');
+    
+    try {
+        const response = await fetch('YOUR_BACKEND_ENDPOINT', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ question: question })
+        });
+
+        const data = await response.json();
+        responseParagraph.innerText = data.answer;
+    } catch (error) {
+        responseParagraph.innerText = 'Error: ' + error.message;
+    }
+}
+
+function downloadCSV() {
+    window.open('YOUR_BACKEND_ENDPOINT_FOR_DOWNLOAD');
+}
